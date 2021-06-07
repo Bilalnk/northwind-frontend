e4542471b0ebc3bff0dfd672571f68b452f2a12e
nadbi <Bilalnk>
2021-06-07 22:52:00 +0300

    intro to react

---
+++ src/layouts/Navi.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Button, Container, Dropdown, Menu } from 'semantic-ui-react'
+import CartSummary from './CartSummary'
+
+function Navi() {
+    return (
+        <div>
+            <Menu inverted fixed="top">
+                <Container>
+
+                    <Menu.Item
+                        name='home'
+
+                    />
+                    <Menu.Item
+                        name='messages'
+
+                    />
+
+                    <Menu.Menu position='right'>
+
+                    <CartSummary></CartSummary>
+
+                        <Menu.Item>
+                            <Button primary>Sign Up</Button>
+                        </Menu.Item>
+                    </Menu.Menu>
+
+                </Container>
+            </Menu>
+        </div>
+    )
+}
+
+export default Navi
